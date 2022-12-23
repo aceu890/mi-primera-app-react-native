98e55013a35876254b42e85192c542330e3efefa
Fernando Andrés - Python Start <72671322+aceu890@users.noreply.github.com>
2022-12-23 17:31:47 -0300

    contador finish

--- src/components/fab.tsx
+++ src/components/fab.tsx
@@ -1,35 +1,68 @@
 import React from 'react'
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet, TouchableNativeFeedback, Platform } from 'react-native';
 
 // Obliga a los usuarios a enviar estos datos obligatoriamente
+// ? = opcional = title?: string 
 interface Props {
-    title: string
+    title: string;
+    position?: 'br' | 'bl';
+    onPress: () => void;
 }
 
-export const Fab = ( { title }: Props ) => {
+// Se puede cambiar el Props a any y ya estariamos trabajando como si fuera javascript
+export const Fab = ( { title, onPress, position = 'br' }: Props ) => {
 
-  return (
-    <TouchableOpacity
-    style={ styles.fabLocationBR}
-    onPress={ () => console.log('click')}
-    >
-    <View style={ styles.fab}>
-        <Text style={ styles.fabText}> { title } </Text>
-    </View>
-    </TouchableOpacity>
-  )
+    const ios = () => {
+        return (
+            <TouchableOpacity
+            onPress={ onPress }
+            activeOpacity={ 0.75 }
+            style={ [
+                styles.fabLocation,
+                (position === 'bl' ) ? styles.left : styles.right
+                ]}
+            >
+      
+                <View style={ styles.fab}>
+                    <Text style={ styles.fabText}> { title } </Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    const android = () => {
+        return (
+            <View
+            style={ [
+                styles.fabLocation,
+                (position === 'bl' ) ? styles.left : styles.right
+            ]}
+        >
+            <TouchableNativeFeedback
+                onPress={ onPress }
+                background={ TouchableNativeFeedback.Ripple('#28425B', false, 30) }
+                >
+                <View style={ styles.fab}>
+                    <Text style={ styles.fabText}> { title } </Text>
+                </View>
+            </TouchableNativeFeedback>
+        </View>
+        )
+    }
+
+  return (Platform.OS === 'ios') ? ios() : android();
 }
 
 const styles = StyleSheet.create({
-    fabLocationBR: {
+    fabLocation: {
         position: 'absolute',
         bottom: 25,
-        right: 25,
     },
-    fabLocationBL: {
-        position: 'absolute',
-        bottom: 25,
-        left: 25,
+    right: {
+        right: 25
+    },
+    left: {
+        left:  25
     },
     fab: {
         backgroundColor:'#5856D6',
@@ -37,6 +70,14 @@ const styles = StyleSheet.create({
         height: 60,
         borderRadius: 100,
         justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+	    width: 0,
+	    height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 8,
     },
     fabText: {
         color:'white',
